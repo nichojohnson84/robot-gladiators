e338dc93ffc3ef20e067e97330f02449510bfec0
nichojohnson84 <nickcjohnson84@outlook.com>
2021-12-21 09:28:10 -0700

    roboSafeUpdate

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a box to enter the name of the robot
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
