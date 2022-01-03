7f8e52aa70c998ad885d977351f2e7f0de39b2de
nichojohnson84 <nickcjohnson84@outlook.com>
2022-01-03 16:21:15 -0700

    restarting debug

--- assets/js/game.js
+++ assets/js/game.js
@@ -81,6 +81,7 @@ var startGame = function() {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
 
+
       // pick new enemy to fight based on the index of the enemyInfo array
       var pickedEnemyObj = enemyInfo[i];
 
